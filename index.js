a2bd957a6ba2b0c9bf54b9bee842b1bb3473845b
gtalin <gtalin@users.noreply.github.com>
2017-01-19 01:07:08 +0000

    Date conversion

--- index.js
+++ index.js
@@ -14,6 +14,37 @@ app.get("/", function(req, res) {
 
 app.get("/:date", function(req, res) {
 	/*Page with JSON data of date*/
-	console.log(req.params);
-	res.send("Will send JSON data here");
+	var date = req.params.date;
+	console.log(req.params)
+	console.log(typeof date);
+	function parseDate(date) {
+		/*var extractedDate = date.replace(/[20%]/g," ");*/
+		var  convertedDate = {unix:null,natural:null};
+		if (isNaN(date)) {
+			console.log("Not unix");
+			console.log(date, Date.parse(date), typeof date);
+			if (Date.parse(date)){
+			  convertedDate.unix = Date.parse(date)/1000;
+			  /*If say date above is of the form "Dec 15" it
+			  is converted to 1008374400000 in node. In Firefox
+			  though it gives NaN Thus we convert date as follows 
+			  to get the same result as from the fcc demo*/
+			  /*convertedDate.natural = date;*/
+			  var newd = new Date(convertedDate.unix*1000)
+			  convertedDate.natural=newd.toDateString().split(" ").slice(1).join(" ");
+		    }
+		}
+		else {
+			console.log("Unix");
+			console.log(new Date(parseInt(date)*1000));
+			/*Coz it expects unix without seconds*/
+			convertedDate.unix = date;
+			var newd = new Date(parseInt(date)*1000)
+			convertedDate.natural=newd.toDateString().split(" ").slice(1).join(" ")
+			/*convertedDate.natural = newd.toDateString();*/
+		}
+		return convertedDate
+	}
+	var converted = parseDate(date);
+	res.send(converted);
 });
